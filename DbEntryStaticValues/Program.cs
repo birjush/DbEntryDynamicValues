d3697dbb928c94024724a2dc75ded96fb72c71ba
BIRJU <BIRJU@DESKTOP-K8UV98I>
2022-08-26 23:13:12 -0700

    Add project files.

---
+++ DbEntryStaticValues/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbEntryStaticValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection sqlConnection;
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\BIRJU\source\repos\DbEntryStaticValues\DbEntryStaticValues\Database1.mdf;Integrated Security=True";
+            try
+            {
+                sqlConnection = new SqlConnection(connectionString);
+                sqlConnection.Open();
+                Console.WriteLine("connection established successfully!");
+                //create crud
+                Console.WriteLine("enter name :");
+                String userName = Console.ReadLine();
+                Console.WriteLine("enter age :");
+                String userAge = Console.ReadLine();
+                string insertQuery = $"INSERT INTO DETAIL(name,age) VALUES ('{userName}','{userAge}')";
+                SqlCommand insertCommand = new SqlCommand(insertQuery, sqlConnection) ;
+                insertCommand.ExecuteNonQuery();
+                Console.WriteLine("data inserted successfully");
+                sqlConnection.Close();
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
